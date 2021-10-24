4e0c0f790ae3cf6bc8aae3ee48f413eb0eae45c7
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2021-10-25 01:02:33 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -41,11 +41,12 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
         (*alerters[1])();
     }
 }
-
-void emailAlerter (void){
 int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+void emailAlerter (void){
+ emailAlertCallCount ++;
 }
 
 void ledAlerter (void){
-int ledAlertCallCount = 0;
+int ledAlertCallCount ++;
 }

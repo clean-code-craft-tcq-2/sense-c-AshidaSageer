99a847752b3f389371e00b1261a9c586ee2fe56b
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2021-10-25 01:12:46 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,7 +3,8 @@ struct Stats {
   float min;
   float max;
 };
-struct Stats compute_statistics( float* numberset, int setlength);
+
+extern struct Stats compute_statistics( float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);

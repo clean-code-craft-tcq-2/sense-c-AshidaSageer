dacc2471d78da36859693b34703d4a093e9aa651
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2021-10-25 12:05:33 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,13 +7,13 @@ struct Stats {
   float max;
 };
 
-struct Stats compute_statistics( float* numberset, int setlength);
+extern struct Stats compute_statistics( float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-void emailAlerter (void);
-void ledAlerter (void);
+extern void emailAlerter (void);
+extern void ledAlerter (void);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
